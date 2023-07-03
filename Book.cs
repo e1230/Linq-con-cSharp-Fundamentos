1b3b0cb40c888ffd4c40d07e71f307ca0025eab7
E1230 <emaurobel1230@hotmail.com>
2023-07-03 10:34:47 -0500

    importación datos

--- Book.cs
+++ Book.cs
@@ -1,11 +1,11 @@
 public class Book
-{    
-    public string Title { get; set; } = "";
-    public int PageCount { get; set; } = 0;
-    public DateTime PublishedDate { get; set; }
-    public DateTime ThumbNailUrl { get; set; }
-    public string ShortDescription { get; set; } = "";
-    public string Status { get; set; } = "";
-    public string[] Authors { get; set; } = Array.Empty<string>();
-    public string[] Categories { get; set; } = Array.Empty<string>();
+{
+  public string Title { get; set; } = "";
+  public int PageCount { get; set; } = 0;
+  public DateTime PublishedDate { get; set; }
+  public string ThumbNailUrl { get; set; } = "";
+  public string ShortDescription { get; set; } = "";
+  public string Status { get; set; } = "";
+  public string[] Authors { get; set; } = Array.Empty<string>();
+  public string[] Categories { get; set; } = Array.Empty<string>();
 }
--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,4 +1,19 @@
+using System.IO;
+using System.Text.Json;
 public class LinqQueries
 {
+  private List<Book> librosCollection = new List<Book>();
 
+  public LinqQueries()
+  {
+    using (StreamReader reader = new StreamReader("books.json"))
+    {
+      string json = reader.ReadToEnd();
+      this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? Enumerable.Empty<Book>().ToList();
+    }
+  }
+  public IEnumerable<Book> TodaLaColeccion()
+  {
+    return librosCollection;
+  }
 }
